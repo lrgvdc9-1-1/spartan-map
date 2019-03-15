9478f717629d2937cfd34670b8f91b2e4601e3aa
hchapa <hchapa@lrgvdc911.org>
2019-03-15 11:43:57 -0500

    make change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { ResizableModule } from 'angular-resizable-element';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EsriComponent } from './map/esri/esri.component';
-import { InboxComponent } from './info/inbox/inbox.component';
+import { InboxComponent } from './windows/inbox/inbox.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FormComponent } from './ticket/form/form.component';
-import { ListInboxComponent } from './display/list-inbox/list-inbox.component';
-import { PreviewTicketComponent } from './display/preview-ticket/preview-ticket.component';
-import { PreviewTicketCommentsComponent } from './display/preview-ticket-comments/preview-comments.component';
-import { PreviewSubdivisionComponent } from './display/preview-subdivision/preview-subdivision.component';
+import { ListInboxComponent } from './previews/list-inbox/list-inbox.component';
+import { PreviewTicketComponent } from './previews/ticket/preview-ticket.component';
+import { PreviewTicketCommentsComponent } from './previews/ticket-comments/preview-comments.component';
+import { PreviewSubdivisionComponent } from './previews/subdivision/preview-subdivision.component';
 
 
 @NgModule({
