6eaaec17133b158d8ed76696c7e72b4dab3f0a40
hchapa <hchapa@lrgvdc911.org>
2019-03-15 13:57:50 -0500

    added configuration model for user

--- src/app/model/interface.ts
+++ src/app/model/interface.ts
@@ -40,3 +40,21 @@ export interface COMMENT {
     time_track_int?: number;
     user_id?: number;
 }
+
+
+export interface USER {
+    user_id?: number;
+    first_name?: string;
+    middle_name?: string;
+    last_name?: string;
+    initials?: string;
+    email?: string;
+    icon?: string;
+    organization?: ORGANIZATION;
+}
+
+export interface ORGANIZATION {
+    organization_id?: number;
+    name?: string;
+    address?: string;
+}
