c96b2b6bdecb6fc3147ea47a99e9553dad77f889
HECTOR CHAPA <hchapa@lrgvdc911.org>
2019-03-22 16:59:27 -0500

    new changes done to navigation hack...

--- src/app/pages/navigation/navigation.component.ts
+++ src/app/pages/navigation/navigation.component.ts
@@ -7,7 +7,9 @@ import { ConfigsService } from 'src/app/services/configs.service';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-
+  widthPane: string = "48px";
+  subtrack: number = 50;
+  widthContent: string = "";
   displayProfile: boolean = false;
   constructor(public configs: ConfigsService) { 
 
@@ -15,6 +17,25 @@ export class NavigationComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.widthContent = (document.body.clientWidth - this.subtrack) + "px";
+  }
+  onCSS() {
+    this.displayProfile = !this.displayProfile;
+    if(this.displayProfile) {
+        this.widthPane = "280px";
+        this.subtrack = 280;
+        this.widthContent = (document.body.clientWidth - this.subtrack) + "px";
+    }
+    else {
+      this.widthPane = "48px";
+      this.subtrack = 50;
+      this.widthContent = (document.body.clientWidth - this.subtrack) + "px";
+    }
   }
 
+  onResize(event) {
+      console.log(event.target.innerWidth);
+      this.widthContent = (event.target.innerWidth - this.subtrack) + "px";
+      console.log(this.widthContent);
+  }
 }
