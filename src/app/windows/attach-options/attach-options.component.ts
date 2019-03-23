24d1aebc9b4d7e4bbb0d9606a1dd7e03d03d8c25
Programmer <programmer@Programmers-MBP.T-mobile.com>
2019-03-23 16:36:50 -0500

    attach component started

--- src/app/windows/attach-options/attach-options.component.ts
+++ src/app/windows/attach-options/attach-options.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./attach-options.component.css']
 })
 export class AttachOptionsComponent implements OnInit {
-
+  public style: any = {};
+  visible: boolean = true;
   constructor() { }
 
   ngOnInit() {
+
+    this.style = {
+      position: "absolute", zIndex: 2,
+      top: "50%", 
+      left: "50%",
+      bottom: "30px",
+      width: "400px", 
+      height: "300px",
+      marginLeft: "-200px",
+      marginTop: "-150px"
+    }    
   }
 
+  getHeight() {
+    this.visible = !this.visible;
+    if(this.visible) {
+      this.style.height = "300px";
+    } else {
+      this.style.height = "30px";
+    }
+  }
 }
