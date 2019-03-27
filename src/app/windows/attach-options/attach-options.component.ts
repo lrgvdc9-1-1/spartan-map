1bfe0d3741d72874cb25d7aa69f4064c5d345922
Programmer <programmer@Programmers-MacBook-Pro.local>
2019-03-27 16:58:52 -0500

    new changes

--- src/app/windows/attach-options/attach-options.component.ts
+++ src/app/windows/attach-options/attach-options.component.ts
@@ -46,6 +46,8 @@ export class AttachOptionsComponent implements OnInit {
 
   onClose() {
     this.esriMap.setMapCursor("default");
+    this.esriMap.setDrawingOption(0);
+    this.esriMap.disAttach();
     this.close.emit(false);
   }
 
