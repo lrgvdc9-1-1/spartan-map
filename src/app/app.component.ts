d2872ffcb162df1e0a88c393f5372da2ac27e0c0
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-01-03 01:49:52 -0600

    remote the ipc not needed it here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,6 @@ import { config } from 'rxjs';
 export class AppComponent {
     constructor(public esriService: EsriService, public configs: ConfigsService){
    
-      //If the communication pipe exists then listen for updates..
-     if(window['ipc']){
-       //get user information when login to the app..
-       window['ipc'].on('get-user-info', (event, data:any) => {
-          console.log("USER INFORMATION IS");
-          console.log(data);
-          configs.selfuser.setUserId(data.user_id);
-          console.log(configs.selfuser);
-          
-       });
-     }
+    
   }
 }
