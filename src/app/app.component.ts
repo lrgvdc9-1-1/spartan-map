bdf20d14019b6dbfb20a96f24ec5b281f634acdb
lrgvdc9-1-1 <hchapa@lrgvdc911.org>
2020-01-03 01:40:50 -0600

    getting user information.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EsriService } from './map/esri.service';
 import { ConfigsService } from './services/configs.service';
+import { config } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,16 @@ import { ConfigsService } from './services/configs.service';
 export class AppComponent {
     constructor(public esriService: EsriService, public configs: ConfigsService){
    
-      //this.configs.start();
+      //If the communication pipe exists then listen for updates..
+     if(window['ipc']){
+       //get user information when login to the app..
+       window['ipc'].on('get-user-info', (event, data:any) => {
+          console.log("USER INFORMATION IS");
+          console.log(data);
+          configs.selfuser.setUserId(data.user_id);
+          console.log(configs.selfuser);
+          
+       });
+     }
   }
 }
